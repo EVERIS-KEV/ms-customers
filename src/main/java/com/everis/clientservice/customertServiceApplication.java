aad493ab6cbdcc8238d117855e062a56109e0137
Mauricio06Everis <kpalmall@everis.com>
2021-08-25 14:22:47 -0500

    Update FINAL

--- src/main/java/com/everis/clientservice/customertServiceApplication.java
+++ src/main/java/com/everis/clientservice/customertServiceApplication.java
@@ -2,14 +2,13 @@ package com.everis.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient; 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient 
+@EnableEurekaClient
 @SpringBootApplication
 public class customertServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(customertServiceApplication.class, args);
-		System.out.println("-Micro servicio cliente, activado.");
-	}
+		public static void main(String[] args) {
+				SpringApplication.run(customertServiceApplication.class , args);
+		}
 }
